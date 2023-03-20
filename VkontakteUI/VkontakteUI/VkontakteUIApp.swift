f5b33a591817aceafcdca2d1a73027bfba88f31e
Mr.Andrew <awpozdnyakov@gmail.com>
2023-03-20 17:43:34 +0300

    Creation a project

---
+++ VkontakteUI/VkontakteUI/VkontakteUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VkontakteUIApp.swift
+//  VkontakteUI
+//
+//  Created by Наталья Рябкова on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct VkontakteUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
